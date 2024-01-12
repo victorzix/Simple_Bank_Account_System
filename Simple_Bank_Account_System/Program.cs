d8b94138531a917559bde18a17e275520f10d0cc
victorzix <linkgamesplays@gmail.com>
2024-01-12 13:53:57 -0300

    Adicionar arquivos de projeto.

---
+++ Simple_Bank_Account_System/Program.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Bank_Account_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
